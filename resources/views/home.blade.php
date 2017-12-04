ded5b575755cb9103144a5ec054f511106b66b06
= <quinlan96@live.com.au>
2017-12-05 05:55:08 +1000

    Added text for no videos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,16 @@
 <div class="container mt-3">
     <div class="row">
 		<ul class="col-md-12" style="margin: 0; padding: 0; display:inline-block; font-size: 0;">
-			@foreach($videos as $video)
-				<li class="col-md-3" style="list-style-type: none; display: inline-block; padding: 10px; margin: 0; font-size: 1rem;">
-					<a href="/video/{{ $video->id }}/"><img src="{{ $video->previewSource() }}" style="width: 100%;"></a>
-					<span style="color: #fff;">{{ $video->name }}</span>
-				</li>
-			@endforeach
+			@if(!empty($videos)
+				@foreach($videos as $video)
+					<li class="col-md-3" style="list-style-type: none; display: inline-block; padding: 10px; margin: 0; font-size: 1rem;">
+						<a href="/video/{{ $video->id }}/"><img src="{{ $video->previewSource() }}" style="width: 100%;"></a>
+						<span style="color: #fff;">{{ $video->name }}</span>
+					</li>
+				@endforeach
+			@else
+				<h3>No videos available</h3>
+			@endif
 		</ul>
     </div>
 </div>
