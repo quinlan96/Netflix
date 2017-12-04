f8651dc14eef3805698f76b8bd5c55bb62a34cf7
= <quinlan96@live.com.au>
2017-12-05 05:57:20 +1000

    For real this time

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <div class="container mt-3">
     <div class="row">
 		<ul class="col-md-12" style="margin: 0; padding: 0; display:inline-block; font-size: 0;">
-			@if(!empty($videos)
+			@if(!empty($videos))
 				@foreach($videos as $video)
 					<li class="col-md-3" style="list-style-type: none; display: inline-block; padding: 10px; margin: 0; font-size: 1rem;">
 						<a href="/video/{{ $video->id }}/"><img src="{{ $video->previewSource() }}" style="width: 100%;"></a>
@@ -12,7 +12,7 @@
 					</li>
 				@endforeach
 			@else
-				<h3>No videos available</h3>
+				<h3 style="text-align: center">No videos available</h3>
 			@endif
 		</ul>
     </div>
